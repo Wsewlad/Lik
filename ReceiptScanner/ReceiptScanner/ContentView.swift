ca03d4815310cf16474d908eacc656aeb95e5e96
vfil <wladyslawfil@gmail.com>
2023-01-14 15:30:26 +0200

    Create Project

---
+++ ReceiptScanner/ReceiptScanner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ReceiptScanner
+//
+//  Created by  Vladyslav Fil on 14.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReceiptScanner/ReceiptScanner/ReceiptScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptScannerApp.swift
+//  ReceiptScanner
+//
+//  Created by  Vladyslav Fil on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
