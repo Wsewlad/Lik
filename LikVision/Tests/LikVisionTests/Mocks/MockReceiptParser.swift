4b76abce2395787509a3ee49ea30ec4f9cd3a9df
vfil <wladyslawfil@gmail.com>
2023-05-23 15:05:45 +0300

    Add scanned result test when no customWords comfiguration provided

--- LikVision/Tests/LikVisionTests/Mocks/MockReceiptParser.swift
+++ LikVision/Tests/LikVisionTests/Mocks/MockReceiptParser.swift
@@ -10,11 +10,31 @@ import Vision
 import XCTest
 
 class MockReceiptParser: RecognizedTextDataSourceDelegate {
+    // Paramethers to test
     var isParseMethodCalled: Bool = false
+    var concatenatedResult: String = ""
     let expectation = XCTestExpectation(description: "Call parse method.")
+    
+    // Configurations
+    var isConcatenatedResultNeeded: Bool
+    
+    init(isConcatenatedResultNeeded: Bool = false) {
+        self.isConcatenatedResultNeeded = isConcatenatedResultNeeded
+        
+    }
     
     func parse(_ observations: [VNRecognizedTextObservation]) {
         isParseMethodCalled = true
+        
+        if isConcatenatedResultNeeded {
+            concatenatedResult = observations.reduce("") {
+                var text = $0
+                text += "\n" + $1.topCandidates(1).first!.string
+                
+                return text
+            }
+        }
+        
         expectation.fulfill()
     }
 }
--- LikVision/Tests/LikVisionTests/TextScannerTests.swift
+++ LikVision/Tests/LikVisionTests/TextScannerTests.swift
@@ -11,17 +11,20 @@ import XCTest
 final class TextScannerTests: XCTestCase {
     
     var sut: TextScanner!
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         sut = TextScanner()
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sut = nil
     }
+}
 
+//MARK: - Delegate's parse method call
+extension TextScannerTests {
     func testTextScanner_whenParseDataFromUIImageMethodCalled_delegateParseMethodShouldBeCalled() throws {
         // Arrange
         let mockReceiptParser = MockReceiptParser()
@@ -35,5 +38,24 @@ final class TextScannerTests: XCTestCase {
         // Assert
         wait(for: [mockReceiptParser.expectation], timeout: 8)
         XCTAssertTrue(mockReceiptParser.isParseMethodCalled, "The delegate's parse method should be called.")
+    }
+}
+
+//MARK: - TextScanner observations result without CustomWords configured
+extension TextScannerTests {
+    func testTextScanner_observationsResultWithoutCustomWordsConfigured_shouldBeTheSame() throws {
+        // Arrange
+        let mockReceiptParser = MockReceiptParser(isConcatenatedResultNeeded: true)
+        sut.delegate = mockReceiptParser
+        
+        let image = try XCTUnwrap(UIImage(named: "silpo-1", in: Bundle.module, compatibleWith: nil))
+        let expectedResult = ExpectedResultExampleSilpo1.withoutCustomWordsConfigured.rawValue
+        
+        // Act
+        sut.parseData(from: image)
+        
+        // Assert
+        wait(for: [mockReceiptParser.expectation], timeout: 8)
+        XCTAssertEqual(mockReceiptParser.concatenatedResult, expectedResult, "The TextScanner observations result should equal to expected one.")
     }
 }
