5c47260357b332000963632e1d3a4967a2f90aff
vfil <wladyslawfil@gmail.com>
2023-06-13 11:36:21 +0300

    Fix product init from matched data

--- LikParsing/Sources/LikParsing/ReceiptParser.swift
+++ LikParsing/Sources/LikParsing/ReceiptParser.swift
@@ -18,13 +18,13 @@ public class ReceiptParser: ReceiptParserProtocol {
         let shop = text.firstMatch(of: tovRegexDSL)?.output.1 ?? "Unknown"
         let date = text.firstMatch(of: dateRegex)?.output.1 ?? Date()
         let sum = text.firstMatch(of: sumRegex)?.output.1 ?? 0
-        let products = text.matches(of: productRegex).compactMap {
+        let products = text.matches(of: productRegex).map { match in
             Product(
-                name: "\($0.1)",
-                amount: $0.2 ?? 1,
-                amountType: $0.2 != nil ? .kg : .piece,
-                price: $0.3 ?? $0.4,
-                sum: $0.4
+                name: "\(match.output.1)",
+                amount: match.output.2 ?? 1,
+                amountType: match.output.2 != nil ? .kg : .piece,
+                price: match.output.3 ?? match.output.4,
+                sum: match.output.4
             )
         }
         
